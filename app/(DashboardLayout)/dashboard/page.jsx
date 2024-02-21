b5b3aae7df95f8f88cdc8fce3c040e44db2cc5bf
bizarreuproar <emirys774@gmail.com>
2024-02-21 15:22:38 +0300

    Add dynamic inputs to dashboard page

--- app/(DashboardLayout)/dashboard/page.jsx
+++ app/(DashboardLayout)/dashboard/page.jsx
@@ -1,7 +1,34 @@
+import Input from '@/components/dashboard/Input'
+const data = [
+    {
+        id: 1,
+        name: "name",
+        type: "text",
+        text: "Name"
+    },
+    {
+        id: 2,
+        name: "color",
+        type: "color",
+        text: "Color"
+    },
+    {
+        id: 3,
+        name: "image",
+        type: "file",
+        text: "Image"
+    }
+]
 export const Page = () => {
     return <div className="flex items-center justify-center h-full w-full">
         <span className="text-4xl font-bold">
-        Welcome to the Gabba Dashboard
+        {data.map((input) => (
+        <Input
+            key={input.id}
+            name={input.name}
+            type={input.type}
+            text={input.text}
+        />))}
         </span>
     </div>
 }
---
+++ components/dashboard/Input.jsx
@@ -0,0 +1,36 @@
+'use client'
+import React, { useState } from 'react';
+
+function Input({ name, type, text }) {
+    const [isInputFocused, setIsInputFocused] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const isInputFilled = inputValue.length > 0 || isInputFocused;
+
+    return (
+        <div>
+            <div className='relative group'>
+                <label htmlFor={name}
+                    className={`absolute left-4 top-3 select-none text-lightGray ${isInputFilled ? 'top-[-6px] text-xs px-1 left-[13px] bg-white' : 'text-base'} transition-all duration-200`}
+                >{text}</label>
+                <input 
+                    className='w-full text-darkGray border rounded-md h-[50px] px-4 border-darkGray focus:border-[2px] focus:outline-none focus:ring-0 focus:border-primary' 
+                    type={type} 
+                    autoComplete="off" 
+                    id={name} 
+                    name={name}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onFocus={() => setIsInputFocused(true)}
+                    onBlur={() => setIsInputFocused(false)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Input
