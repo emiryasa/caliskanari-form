e040b900664f8a456ee9654d82934a7f319f775a
senayakagunduz <senayakgndz@gmail.com>
2024-02-21 14:05:39 +0300

    our clients section

--- components/clients-section/clients-section.jsx
+++ components/clients-section/clients-section.jsx
@@ -1,22 +1,24 @@
 "use client"
 import { info, image } from "../constants/index";
 import { ClientSlider } from "./clients-slider";
+import EditableTextComponent from "./editable-component";
 export const ClientsSection = () => {
   const { clientTitle1, clientTitle2, clientDesc } = info;
   const { underline } = image;
+
   return (
     <div className="bg-cream pt-24">
       <div className="mx-auto container p-6 relative">
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-cst_grey text-sm p-1">{clientTitle1}</p>
+        <div className="flex flex-col items-center justify-center">  
+            <EditableTextComponent className="text-cst_grey text-sm p-1" initialText={clientTitle1} />
           <div className="text-cst_purple text-4xl font-semibold relative pb-6">
-            <h2>{clientTitle2}</h2>
+            <EditableTextComponent className="" initialText={clientTitle2} />
             <img src={underline} alt="" className="absolute right-7" />
           </div>
-          <p className="text-cst_grey text-base">{clientDesc}</p>
+          <EditableTextComponent className="text-cst_grey text-base" initialText={clientDesc} />
         </div>
         <img
-        className="absolute -top-14 right-3"
+          className="absolute -top-14 right-3"
           src="https://www.radiustheme.com/demo/wordpress/themes/quiklearn/wp-content/themes/quiklearn/assets/element/red-star.svg" alt=""
         />
       </div>
