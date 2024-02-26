e0800101faf2a3113f5526e1abcafbab25c1fda7
bizarreuproar <emirys774@gmail.com>
2024-02-26 21:03:10 +0300

    Refactor info-section component and update Zustand store

--- components/info-section/info-section.jsx
+++ components/info-section/info-section.jsx
@@ -1,24 +1,34 @@
 import { informations } from "../constants/index"
 import { motion } from "framer-motion"
 import "./info-section.css"
+import { useStore } from "@/zustand/index"
 
 export const InfoSection = () => {
+  const infoSection = useStore((state) => state.infoSection) || [];
+
   return (
     <div className="bg-cst_purple info-section bg-center bg-cover py-16">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:p-6 text-white">
-        {informations.map((item) => (
-          <motion.div key={item.title}  className="hoverable-div">
-            <div className="flex items-center gap-4 py-5 px-8 info-section-icons">
-              <motion.span  className={`text-4xl ${item.color}  `}>
-                {item.icon && <item.icon />}
-              </motion.span>
-              <span className="flex flex-col gap-2">
-                <p className="text-xl">{item.title}</p>
-                <p className="text-base text-lightgrey">{item.description}</p>
-              </span>
-            </div>
-          </motion.div>
-        ))}
+        {informations.map((item, index) => {
+          const textColor = infoSection[index]?.textColor || ''; // Dynamically set textColor for each item
+          return (
+            <motion.div key={item.title} className="hoverable-div">
+              <div className="flex items-center gap-4 py-5 px-8 info-section-icons">
+                <motion.span className={`text-4xl ${item.color}`}>
+                  {item.icon && <item.icon />}
+                </motion.span>
+                <span className="flex flex-col gap-2">
+                  {index === 0 && <p className={`text-xl ${textColor}`}>{infoSection[0]?.text}</p>}
+                  {index === 0 && <p className="text-base text-lightgrey">{infoSection[1]?.text}</p>}
+                  {index === 1 && <p className={`text-xl ${textColor}`}>{infoSection[2]?.text}</p>}
+                  {index === 1 && <p className="text-base text-lightgrey">{infoSection[3]?.text}</p>}
+                  {index === 2 && <p className={`text-xl ${textColor}`}>{infoSection[4]?.text}</p>}
+                  {index === 2 && <p className="text-base text-lightgrey">{infoSection[5]?.text}</p>}
+                </span>
+              </div>
+            </motion.div>
+          );
+        })}
       </div>
     </div>
   )
--- zustand/index.js
+++ zustand/index.js
@@ -1,6 +1,9 @@
 'use client'
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
+import { FcIdea } from 'react-icons/fc'
+import { GiTiedScroll } from 'react-icons/gi'
+import { TfiCup } from 'react-icons/tfi'
 
 export const useStore = create(persist((set) => ({
     searchBar: [
@@ -12,14 +15,19 @@ export const useStore = create(persist((set) => ({
         { name: "button", textColor: '#000000', buttonColor: "#0169fd", hoverColor: "#001e9d", text: 'Etkileşimli Alıştırmaları Keşfet' },
     ],
     infoSection: [
-        { name: "header", textColor: '#000000', text: 'Section 1' },
-        { name: "text", textColor: '#000000', text: 'Section 2' },
-        { name: "button", textColor: '#000000', text: 'Section 3' },
-    ],
+        { name: "header", textColor: '#f8fafc', text: 'Etkileşimli öğrenme', icon: GiTiedScroll,},
+        { name: "text", textColor: '#f8fafc', text: 'Öğrencilerin aktivite ve oyunlar ile hem eğlendikleri hem de öğrendikleri eğitim sistemi' },
+       
+        { name: "headerSecond", textColor: '#f8fafc', text: 'İstatistikler ve Analizler', icon: FcIdea}, 
+        { name: "textSecond", textColor: '#f8fafc', text: 'Öğrencilerin etkileşimlerdeki başarı oranları, istatistikleri veren ve takip eden öğretmenlere özel alt yapı' },
+    
+        { name: "headerThird", textColor: '#f8fafc', text: 'Öğretmenlere özel Altyapı', icon: TfiCup },
+        { name: "textThird", textColor: '#f8fafc', text: 'Öğretmenlerin kendi sınıflarını oluşturup öğrenciler ile sınıf süreçlerini yönetebildiği altyapı' },
+      ],
     topClassCourses: [
-        { name: "header", textColor: '#000000', text: 'Section 1' },
-        { name: "text", textColor: '#000000', text: 'Section 2' },
-        { name: "button", textColor: '#000000', text: 'Section 3' },
+        { name: "headerTop", textColor: '#000000', text: 'EĞLENDİRİRKEN ÖĞRETEN UYGULAMA' },
+        { name: "headerMain", textColor: '#000000', text: 'Şimdi Eğitici Alıştırmaları Keşfet' },
+        { name: "description", textColor: '#000000', text: 'Sınıf seviyenize müfredat ilerleyişine uygun ilerleyiş ve %100 MEB uyumlu alıştırmalar' },
     ],
     learnerStudents: [
         { name: "header", textColor: '#000000', text: 'Section 1' },
